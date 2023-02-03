aa53b0014d791c58d250a40c0cfa1e9688f38285
Oihana <oihana.darrieumerlou@gmail.com>
2023-02-03 12:10:00 +0100

    start portfolio with loader

---
+++ main.js
@@ -0,0 +1,18 @@
+const loaderContainer = document.getElementsByClassName('container');
+
+
+function fade(element) {
+    let op = 1;  // initial opacity
+    let timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+setTimeout(() => {
+    fade(loaderContainer[0])
+  }, 2000);
